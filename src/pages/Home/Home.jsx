2aa99856d729dd1697b497bc8a01bdcd36fc331a
Jose Luis <joseluisblascoortiz@hotmail.com>
2021-04-30 00:51:28 +0200

    cocktail card 100%

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -149,49 +149,185 @@ export default class Home extends Component {
             <h1>Cocktails</h1>
             <div class="wrapper-cart">
               <div class="list">
-                <h2 class="list-title">Cocktails</h2>
+                <h2 class="list-title">Los Mala Madre</h2>
                 <ul>
                   <li class="item">
-                    <span class="name">Manhatan</span>
-                    <span class="price">22</span>
+                    <span class="name">PIÑA CORN</span>
+                    <span class="price">9</span>
                   </li>
+                  <p>Ron,piña,coco,palomitas</p>
                   <li class="item">
-                    <span class="name">Pipireto</span>
-                    <span class="price">22</span>
+                    <span class="name">DONATELLA</span>
+                    <span class="price">8</span>
                   </li>
+                  <p>Gin,violeta,limón.</p>
                   <li class="item">
-                    <span class="name">Piña colada</span>
-                    <span class="price">19</span>
+                    <span class="name">SOUTH SIDE</span>
+                    <span class="price">8</span>
                   </li>
+                  <p>Gin,limón,hierbabuena</p>
                   <li class="item">
-                    <span class="name">Sandia Boom</span>
-                    <span class="price">22</span>
+                    <span class="name">PORN STAR</span>
+                    <span class="price">9</span>
                   </li>
+                  <p>Vodka,maracuyá,lima,vainilla</p>
                   <li class="item">
-                    <span class="name">Caipirinha</span>
-                    <span class="price">14</span>
+                    <span class="name">JUNE BUG</span>
+                    <span class="price">9</span>
                   </li>
+                  <p>Platanca,coco,piña,melón</p>
                   <li class="item">
-                    <span class="name">Manhatan</span>
-                    <span class="price">22</span>
+                    <span class="name">PENICILIN</span>
+                    <span class="price">9</span>
                   </li>
+                  <p>Whisky ahumado,jengibre,miel</p>
                   <li class="item">
-                    <span class="name">Pipireto</span>
-                    <span class="price">22</span>
+                    <span class="name">ROSAS & ENCANTO</span>
+                    <span class="price">9</span>
                   </li>
+                  <p>Vodka,arándanos,frambuesa,lima</p>
                   <li class="item">
-                    <span class="name">Piña colada</span>
-                    <span class="price">19</span>
+                    <span class="name">MAI"MADRE"</span>
+                    <span class="price">9</span>
                   </li>
+                  <p>Ron,piña,coco,jengibre</p>
                   <li class="item">
-                    <span class="name">Sandia Boom</span>
-                    <span class="price">22</span>
+                    <span class="name">SOON OF JAMES</span>
+                    <span class="price">9</span>
                   </li>
-                  <li class="item">
-                    <span class="name">Caipirinha</span>
-                    <span class="price">14</span>
-                  </li>
+                  <p>Jameson,lima,naranja</p>
                 </ul>
+
+                <h2 class="list-title h2-title">Los Clásicos</h2>
+                <ul>
+                  <li class="item">
+                    <span class="name">MOJITO</span>
+                    <span class="price">6</span>
+                  </li>
+                  <p>Ron,azúcar,menta</p>
+                  <li class="item">
+                    <span class="name">MOJITO SABORES</span>
+                    <span class="price">7</span>
+                  </li>
+                  <p>Fresa,sandía,piña,coco</p>
+                  <li class="item">
+                    <span class="name">DAIQUIRI SABORES</span>
+                    <span class="price">7</span>
+                  </li>
+                  <p>Fresa,sandía,piña,coco</p>
+                  <li class="item">
+                    <span class="name">PIÑA COLADA</span>
+                    <span class="price">7</span>
+                  </li>
+                  <p>Ron,piña,coco</p>
+                  <li class="item">
+                    <span class="name">S**  ON THE BEACH</span>
+                    <span class="price">7</span>
+                  </li>
+                  <p>Vodka,arándanos,naranja</p>
+                  <li class="item">
+                    <span class="name">DARK & STORMY</span>
+                    <span class="price">8</span>
+                  </li>
+                  <p>Ron,jengibre,coco</p>
+                  <li class="item">
+                    <span class="name">MOSCOW MULE</span>
+                    <span class="price">7</span>
+                  </li>
+                  <p>Vodka,jengibre,lima</p>
+                  <li class="item">
+                    <span class="name">OLD SCHOOL</span>
+                    <span class="price">9</span>
+                  </li>
+                  <p>Whisky,vainilla,palomitas</p>
+                </ul>
+
+                <h2 class="list-title h2-title">Los Toro Rosso</h2>
+                <ul>
+                  <li class="item">
+                    <span class="name">DARK & STORMY</span>
+                    <span class="price">7</span>
+                  </li>
+                  <p>Ron,jengibre,cola,original RB</p>
+                  <li class="item">
+                    <span class="name">GIN TONIC</span>
+                    <span class="price">7</span>
+                  </li>
+                  <p>Gin,tónica,original RB</p>
+                  <li class="item">
+                    <span class="name">WHISKY ALE</span>
+                    <span class="price">7</span>
+                  </li>
+                  <p>Whisky,ginger,limón,original RB</p>
+                  <li class="item">
+                    <span class="name">GIN LEMON</span>
+                    <span class="price">7</span>
+                  </li>
+                  <p>Gin,limón,original RB</p>
+                  </ul>
+
+                  <h2 class="list-title h2-title">For Drivers</h2>
+                <ul>
+                  <li class="item">
+                    <span class="name">GINGER PUMPKING</span>
+                    <span class="price">7</span>
+                  </li>
+                  <p>Calabaza,canela,naranja</p>
+                  <li class="item">
+                    <span class="name">MOJITO 0,0</span>
+                    <span class="price">7</span>
+                  </li>
+                  <p>Hierbabuena,lima</p>
+                  <li class="item">
+                    <span class="name">SAN FRANCISCO</span>
+                    <span class="price">7</span>
+                  </li>
+                  <p>Naranja,piña,melocotón</p>
+                  </ul>
+
+                  <h2 class="list-title h2-title">Aperitivo</h2>
+                <ul>
+                  <li class="item">
+                    <span class="name">NEGRONI</span>
+                    <span class="price">6</span>
+                  </li>
+                  <p>Gin,vermut,Campari</p>
+                  <li class="item">
+                    <span class="name">AMERICANO</span>
+                    <span class="price">6</span>
+                  </li>
+                  <p>Soda,vermut,Campari</p>
+                  <li class="item">
+                    <span class="name">MARGARITA</span>
+                    <span class="price">7</span>
+                  </li>
+                  <p>Tequila,triple seco,lima</p>
+                  <li class="item">
+                    <span class="name">PISCO SOUR</span>
+                    <span class="price">8</span>
+                  </li>
+                  <p>Pisco,limón,huevo</p>
+                  </ul>
+
+                  <h2 class="list-title h2-title">Batidos De Fruta</h2>
+                <ul>
+                  <li class="item">
+                    <span class="name">PIÑA & COCO</span>
+                    <span class="price">6</span>
+                  </li>
+                  <p>Leche y azúcar</p>
+                  <li class="item">
+                    <span class="name">ARÁNDANOS & MENTA</span>
+                    <span class="price">6</span>
+                  </li>
+                  <p>Leche y azúcar</p>
+                  <li class="item">
+                    <span class="name">NARANJA & FRESA</span>
+                    <span class="price">6</span>
+                  </li>
+                  <p>Zumo y lima</p>
+                  </ul>
+
               </div>
             </div>
           </section>
