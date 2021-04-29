f28ab88151927d20980d24051881adc49fa4e8f7
Jose Luis <joseluisblascoortiz@hotmail.com>
2021-04-29 23:58:43 +0200

    fav icon ok

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@
 import './App.css';
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 import Home from "./pages/Home/Home.jsx";
-import Clock from "./components/Clock.jsx";
+
 
 function App() {
   
@@ -11,7 +11,6 @@ function App() {
 <BrowserRouter>
 <Switch>
           <Route exact path="/" component={Home}/>
-          <Route exact path="/clock" component={Clock}/>
         </Switch>
 </BrowserRouter>
     </div>
